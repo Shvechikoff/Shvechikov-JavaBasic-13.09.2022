7e13b96e2fa65d496748d08389eb18f02c7b9e86
Illia <illia.shvechikov@gmail.com>
2022-09-14 20:19:23 +0300

    first commit

---
+++ src/location/Main.java
@@ -0,0 +1,15 @@
+package location;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double Latitude =  49.4420548161306;
+
+        double Longitude = 32.08257846983652;
+
+        System.out.println("Location:" + "Cherkasy" +
+                "\n" + "Latitude:" + Latitude +
+                "\n" + "Longitude:" + Longitude );
+
+    }
+}
